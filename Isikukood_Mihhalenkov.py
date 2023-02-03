1d631826f8852d38f3e676f9fa33e47bec6af192
ArtemiMihhalenkov <120027616+Sufed@users.noreply.github.com>
2023-02-03 13:27:14 +0200

    Update Isikukood_Mihhalenkov.py

--- Isikukood_Mihhalenkov.py
+++ Isikukood_Mihhalenkov.py
@@ -1,1 +1,15 @@
-﻿isikukood=int(input("Введите isikukood: "))
+isikukood=[]
+while True:
+    isikukood=input("Введите isikukood: ")
+    print("!Isikukood kontroll!")
+    if len(isikukood)==11 and isikukood.isdigit():
+        isikukood_list=list(isikukood) #Выводим первую цифру.
+        первая_цифра=isikukood_list[0] 
+        print("Первая цифра:", первая_цифра)
+        if первая_цифра in ["1","2","3","4","5","6"]: #Квадратные скобки нужны чтобы проверить содержутся ли эти цифры в первой букве.
+            print("Isikukood on correct")
+            break
+        else:
+            print("Isikukood on viga")
+    else:
+        print("Неверно написан isikukood.") #Выдаст если написать буквами или если цифр меньше чем 11 и наоборот.
