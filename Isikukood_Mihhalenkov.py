58df2d852110a4aae3252baf903a698a6e587f8d
ArtemiMihhalenkov <120027616+Sufed@users.noreply.github.com>
2023-02-06 19:05:37 +0200

    Update Isikukood_Mihhalenkov.py

--- Isikukood_Mihhalenkov.py
+++ Isikukood_Mihhalenkov.py
@@ -43,3 +43,45 @@ else:
     print(da)
     d=da/11
     print(d)
+    f=d*11
+    print(f)
+    h=s-f
+    print(h)
+    
+    hhh=int(ik_list[8]+ik_list[9]+ik_list[10])
+    if 1<=hhh<=10:
+        haigla="kuresaare Higla"
+    elif 11<=hhh<=19:
+        haigla="Tartu Ülikooli Naistekliinik, Tartumaa, Tartu"
+    elif 21<=hhh<=220:
+        haigla="ida-Tallinna Keskhaigla, Pelgulinna sünnitusmaja, Hiiumaa, Keila, Rapla haigla, Loksa haigla"
+    elif 221<=hhh<=270:
+        haigla="Ida-Viru Keskhaigla (Kohtla-Järve, endine Jõhvi)"
+    elif 271<=hhh<=370:
+        haigla="Maarjamõisa Kliinikum (Tartu), Jõgeva Haigla"
+    elif 371<=hhh<=420:
+        haigla="Narva Haigla"
+    elif 471<=hhh<=490:
+        haigla="Pärnu Haigla"
+    elif 491<=hhh<=520:
+        haigla="Pelgulinna Sünnitusmaja (Tallinn), Haapsalu haigla"
+    elif 521<=hhh<=570:
+        haigla="Järvamaa Haigla (Paide)"
+    elif 571<=hhh<=600:
+        haigla="Valga Haigla"
+    elif 601<=hhh<=650:
+        haigla="Viljandi Haigla"
+    elif 651<=hhh<=700:
+        haigla="Lõuna-Eesti Haigla (Võru), Põlva Haigla "
+        print(haigla)
+        if int(ik_list[0])%2==0:
+            sugu="naine"
+   else:
+        sugu="mees"
+        print(sugu)
+        print(f"see in {sugu}, sünnipäev {spaev} Ta on sündinud {haigla}")
+        isikukoodid.append(ik)
+print(isikukoodid)
+arvud=list(map(int,arvud))
+arvud.sort()
+print(arvud)
