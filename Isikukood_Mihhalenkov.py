85231043e918df821dd97f80395741ccd0164a4f
ArtemiMihhalenkov <120027616+Sufed@users.noreply.github.com>
2023-02-07 16:57:40 +0200

    Update Isikukood_Mihhalenkov.py

--- Isikukood_Mihhalenkov.py
+++ Isikukood_Mihhalenkov.py
@@ -1,86 +1,100 @@
-﻿def Sugu(ik_list:list)->str:
-    """Esimese tahe järgi määrme sugu
-    :param list ik_list:Järjend isikukoodi numbridest
-    :rtype: str
-    """
+from math import*
+from random import*
 
-    if int(ik_list[0])%2==0:
-        sugu="naine"
-    else:
-        sugu="mees"
-    return sugu
+from OmaMoodul1 import *
 
 
-def Sunnikoht(a:int)->str:
-    """määrme Sunnikoht #......
-    :param int a: Järjend sunnikoht
-    :rtype: str
-    """
-    if 1<=a<=10:
-                    haigla="kuresaare Higla"
-    elif 11<=a<=19:
-                    haigla="Tartu Ülikooli Naistekliinik, Tartumaa, Tartu"
-    elif 21<=a<=220:
-                    haigla="ida-Tallinna Keskhaigla, Pelgulinna sünnitusmaja, Hiiumaa, Keila, Rapla haigla, Loksa haigla"
-    elif 221<=a<=270:
-                    haigla="Ida-Viru Keskhaigla (Kohtla-Järve, endine Jõhvi)"
-    elif 271<=a<=370:
-                    haigla="Maarjamõisa Kliinikum (Tartu), Jõgeva Haigla"
-    elif 371<=a<=420:
-                    haigla="Narva Haigla"
-    elif 471<=a<=490:
-                    haigla="Pärnu Haigla"
-    elif 491<=a<=520:
-                    haigla="Pelgulinna Sünnitusmaja (Tallinn), Haapsalu haigla"
-    elif 521<=a<=570:
-                    haigla="Järvamaa Haigla (Paide)"
-    elif 571<=a<=600:
-                    haigla="Valga Haigla"
-    elif 601<=a<=650:
-                    haigla="Viljandi Haigla"
-    elif 651<=a<=700:
-                    haigla="Lõuna-Eesti Haigla (Võru), Põlva Haigla "
-    return haigla
 
-def Sunnipaev(ik_list:list)->str:
-     """määrme spaev
-     :parem str ik: Järjend spaev
-     :rtype: str
-     """
-     s1=int(ik_list[0])
-     y=ik_list[1]+ik_list[2] #aasta
-     m=ik_list[3]+ik_list[4] #kuu
-     d=ik_list[5]+ik_list[6] #päev
+<<<<<<< HEAD
+
+=======
+>>>>>>> 52707ba6393f937a9c1bf7f212912e0e1403909d
+arvud=[]
+isikukoodid=[]
+
+while True:
+
+   ik=input("Anna isikukood: ") #str
+   if ik == '1': break
+<<<<<<< HEAD
+
+   if ik.isalpha():
+       print("Viga")
+   else:
    
-     if int(m)<1 or int(m)>13 and int(d)<1 or int(d)>31:
-         spaev="Viga"
-     else:
-         if s1==1 or s1==2:
-             yy="18"
-         elif s1==3 or s1==4:
-             yy="19"
-         else:
-             yy="20"
-         spaev=d+"."+m+"."+yy+y
-         return spaev
-
-def naised_mehed(ikoodid:list):
-    """...
-    :rtype: list
-    """
-    naised=[]
-    mehed=[]
-    for kood in ikoodid:
-        ik_list=list(kood)
-        sugu=Sugu(ik_list)
-        if sugu=="naine":
-            naised.append(kood)
+    if len(ik)!=11:   #ne rovno 11
+       print("Liiga palju või liiga vähe sümboleid. Sisesta veel kord: ")
+       arvud.append(ik)
+    else:
+=======
+   
+   if len(ik)!=11:   #ne rovno 11
+       print("Liiga palju või liiga vähe sümboleid. Sisesta veel kord: ")
+       arvud.append(ik)
+   else:
+>>>>>>> 52707ba6393f937a9c1bf7f212912e0e1403909d
+        print("Isikukoodi kontroll")
+        ik_list=list(ik)
+        s1=int(ik_list[0]) #"1"->1
+        if s1 not in [1,2,3,4,5,6]:
+            print("Esimine sümbol ei ole õige!")
+            arvud.append(ik)
         else:
-            mehed.append(kood)
-    ikoodid.clear()
-    ikoodid.extend(naised)
-    ikoodid.extend(mehed)
-    return ikoodid
+<<<<<<< HEAD
+            #
+            print("Esimene sümbol on õige ")
+            spaev=Sunnipaev(ik_list)
+            if spaev=="Viga":
+                arvud.append(ik)
+            
+            else:    
+                #
+                print(f"Sünnipäev on {spaev}")
+                print({f"Viimane number: {ik_list[-1]}"})
+                
+=======
+            print("Esimine sümbol on õige")
+            y=ik_list[1]+ik_list[2] #aasta
+            m=ik_list[3]+ik_list[4] #kuu
+            d=ik_list[5]+ik_list[6] #päev
+            if (int(m)<1 or int(m)>13) and (int(d)<1 or int(d)>31):
+                print("Sünnipäev ei saa luua")
+                arvud.append(ik)
+            else:
+                if s1==1 or s1==2:
+                    yy="18"
+                elif s1==3 or s1==4:
+                    yy="19"
+                else:
+                    yy="20"
+                spaev=str(d)+"."+str(m)+"."+str(y) #ei ole ´18..,19,
+                print(f"Sünnipäev on {spaev}")
+                print({f"Viimane number: {ik_list[-1]}"})
+>>>>>>> 52707ba6393f937a9c1bf7f212912e0e1403909d
+                s=3*int(ik_list[0])+4*int(ik_list[1])+5*int(ik_list[2])+6*int(ik_list[3])+7*int(ik_list[4])+8*int(ik_list[5])+9*int(ik_list[6])+1*int(ik_list[7])+2*int(ik_list[8])+3*int(ik_list[9])
+                print(s)
+                d=s//11
+                print(d)
+                f=d*11
+                print(f)
+                h=s-f
+                print(h)
+
+                hhh=int(ik_list[8]+ik_list[9]+ik_list[10])
+<<<<<<< HEAD
+               # 
+                haigla=Sunnikoht(hhh)
+                
+                sugu=Sugu(ik_list)
+                print(f"see in {sugu}, sünnipäev {spaev} Ta on sündinud {haigla}")
+                isikukoodid.append(ik)
+
+print()
+isikukoodid=naised_mehed(isikukoodid) #sort alguses naised ja pärast mehed
+print(isikukoodid)
+arvud=list(map(int,arvud))       #получаем интовые заначения через map
+arvud.sort()
+print(arvud)
 #-----------
 isikukood=[] #50608167079
 arvud=[]
